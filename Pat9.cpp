bea3c9799d93141e717c31bfa7bf1b11a423919b
AnugyaBajpai2454 <bajpaianugyaab@gmail.com>
2026-01-28 01:21:46 +0530

    Patterns_2

---
+++ Pat9.cpp
@@ -0,0 +1,15 @@
+//Patterns
+#include <iostream>
+using namespace std;
+int main (){
+    int n = 4;
+    for (int i=0; i<n; i++){
+        int count = 1;
+        for(int j = 0; j<i+1; j++){
+            cout << count ++;
+        }
+        cout<< endl;
+    }
+    
+    return 0;
+}
